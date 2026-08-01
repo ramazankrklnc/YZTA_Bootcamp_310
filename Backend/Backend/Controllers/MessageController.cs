52293afe62b5be924ae9795626d4eb8993b73a28
Mert Özyağlı <mertozyagli0@gmail.com>
2026-08-01 15:30:08 +0300

    Mesajlar Güncellendi

---
+++ Backend/Backend/Controllers/MessageController.cs
@@ -0,0 +1,62 @@
+﻿using Backend.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Backend.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class MessageController : ControllerBase
+    {
+
+        private readonly IMessageService _messageService;
+
+
+        public MessageController(
+            IMessageService messageService
+        )
+        {
+            _messageService = messageService;
+        }
+
+
+
+
+
+        [HttpGet("session/{sessionId}")]
+        public async Task<IActionResult> GetMessages(
+            int sessionId
+        )
+        {
+
+            try
+            {
+
+                var result =
+                    await _messageService
+                    .GetMessagesAsync(
+                        sessionId
+                    );
+
+
+                return Ok(result);
+
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(new
+                {
+                    message = ex.Message
+                });
+
+            }
+
+        }
+
+    }
+
+}
---
+++ Backend/Backend/Interfaces/IMessageService.cs
@@ -0,0 +1,13 @@
+﻿using Backend.Entities;
+
+namespace Backend.Interfaces
+{
+    public interface IMessageService
+    {
+
+        Task<List<ChatMessage>> GetMessagesAsync(
+            int sessionId
+        );
+
+    }
+}
